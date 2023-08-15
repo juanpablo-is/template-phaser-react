788916676b337df851f73985e74a8d5c5cec6112
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-14 21:31:18 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
+import { useMemo } from 'react'
 import Phaser from 'phaser'
 import { GameScene } from './scenes/Game'
-import { useMemo } from 'react'
 
 function App () {
-  const scene = useMemo(() => {
+  const game = useMemo(() => {
     return new Phaser.Game({
       type: Phaser.AUTO,
-      parent: 'game',
+      parent: 'root',
       backgroundColor: '#282c34',
       scale: {
         mode: Phaser.Scale.ScaleModes.RESIZE,
         width: window.innerWidth,
         height: window.innerHeight
       },
+      physics: {
+        default: 'arcade',
+        arcade: {
+          debug: true,
+          gravity: { y: 200 }
+        }
+      },
       scene: [GameScene]
     })
   }, [])
 
-  console.log(scene)
-
-  return <div id='game' />
+  return (
+    <div>
+      <button
+        onClick={() => {
+          game.scene.keys.game.spawnHamster()
+        }}
+      >
+        click me
+      </button>
+      <div id='game' />
+    </div>
+  )
 }
 
 export default App
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,20 +1,59 @@
 export class GameScene extends Phaser.Scene {
+  assets = []
+  group
+
   constructor() {
     super("game")
+    this.assets = [
+      "hamster-1",
+      "hamster-2",
+      "hamster-3",
+      "hamster-4",
+      "hamster-5",
+      "hamster-6",
+      "hamster-7",
+      "hamster-8",
+    ]
   }
 
   // Función para precargar assets
   preload() {
+    this.assets.forEach(asset =>
+      this.load.image(asset, `assets/${asset}.png`)
+    )
+  }
 
+  // https://labs.phaser.io/edit.html?src=src/physics/arcade/custom%20separate.js&v=3.60.0
+  separate(sprite1, sprite2) {
+    const b1 = sprite1.body;
+    const b2 = sprite2.body;
+
+    if (b1.y > b2.y) {
+      b2.y += b1.top - b2.bottom;
+      b2.stop()
+    } else {
+      b1.y += b2.top - b1.bottom;
+      b1.stop()
+    }
+  }
+
+  spawnHamster() {
+    const random = this.assets[Math.floor(Math.random() * this.assets.length)]
+
+    const hamster = this.group.create(300, 100, random);
+    hamster.setScale(.1)
+    hamster.setCollideWorldBounds(true);
+    hamster.body.customSeparateY = true;
+
+    this.physics.add.collider(hamster, this.group, this.separate);
   }
 
   // Función para crear la escena
   create() {
-    this.add.text(10, 10, 'Hello, Phaser!')
+    this.physics.world.checkCollision.up = false;
+    this.group = this.physics.add.group();
   }
 
   // Función que actualiza de acuerdo a los frames
-  update() {
-
-  }
+  update() { }
 }
