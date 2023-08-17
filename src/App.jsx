a8287714a10166e3181be0e8e05bfbd20d6e02a0
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-08-17 00:05:00 -0500

    Update dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import Phaser from 'phaser'
 import { GameScene } from './scenes/Game'
 
@@ -6,7 +6,7 @@ function App () {
   const game = useMemo(() => {
     return new Phaser.Game({
       type: Phaser.AUTO,
-      parent: 'root',
+      parent: 'game',
       backgroundColor: '#282c34',
       scale: {
         mode: Phaser.Scale.ScaleModes.RESIZE,
@@ -24,6 +24,10 @@ function App () {
     })
   }, [])
 
+  useEffect(() => {
+    return () => game.destroy(true)
+  }, [game])
+
   return (
     <div>
       <button
@@ -33,6 +37,11 @@ function App () {
       >
         click me
       </button>
+
+      <button onClick={() => game.scene.keys.game.destroyHamster()}>
+        eliminar
+      </button>
+
       <div id='game' />
     </div>
   )
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,6 +2,9 @@ export class GameScene extends Phaser.Scene {
   assets = []
   group
 
+  stackMove = 0 // medida que se deberá mover cada vez que se añade un hamster (la medida es el alto de la imagen)
+  stackDimensions = { width: 0, height: 0 } // el ancho y alto de la pila de imagenes
+
   constructor() {
     super("game")
     this.assets = [
@@ -37,15 +40,36 @@ export class GameScene extends Phaser.Scene {
     }
   }
 
+  destroyHamster() {
+    this.stackMove = 0
+    this.stackDimensions = { width: 0, height: 0 }
+    this.cameras.main.scrollY = 0 // se reinicia la camara en posición inicial 0
+    this.group.clear(true, true)
+  }
+
   spawnHamster() {
+    const { width, height } = this.sys.game.config
+
     const random = this.assets[Math.floor(Math.random() * this.assets.length)]
 
-    const hamster = this.group.create(300, 100, random);
+    const hamster = this.group.create(300, 100 + this.cameras.main.scrollY, random);
     hamster.setScale(.1)
     hamster.setCollideWorldBounds(true);
     hamster.body.customSeparateY = true;
 
     this.physics.add.collider(hamster, this.group, this.separate);
+
+    // Si la mitad del height de la pantalla ya superó el alto de la pila
+    // se empezará a guardar la medida que la camara tiene que moverse (cuando se tiene que mover?, la medida de los hamsters añadidos)
+    // si aún no supera la mitad, la distancia que se tiene que mover es 0 (o sea, la camara sin movimiento)
+    this.stackMove = this.stackDimensions.height >= height / 2 ? hamster.height * hamster.scale : 0
+
+    // Calcula el height y width de la pila de hamster
+    this.stackDimensions = this.group.getChildren().reduce((acc, child) => {
+      acc.width = Math.max(acc.width, child.width * child.scale)
+      acc.height += child.height * child.scale
+      return acc
+    }, { width: 0, height: 0 });
   }
 
   // Función para crear la escena
@@ -55,5 +79,13 @@ export class GameScene extends Phaser.Scene {
   }
 
   // Función que actualiza de acuerdo a los frames
-  update() { }
+  update() {
+    // Si hay un valor que mover en la camara (debe ser superior a 0)
+    // se le restará al scroll en Y de la camara principal
+    // posterior, se setea a 0 el movimiento que tiene que aplicar la camara, esto debido a que ya se aplico el movimiento
+    if (this.stackMove > 0) {
+      this.cameras.main.scrollY -= this.stackMove;
+      this.stackMove = 0
+    }
+  }
 }
